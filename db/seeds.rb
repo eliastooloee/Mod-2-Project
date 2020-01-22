b85b9d46a1e86acd175b85d02887a94b323c4e1c
eliastooloee <elitooloee@gmail.com>
2020-01-22 12:16:38 -0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Rock.create(nick_name: 'Granodiorite', type: 'Igneous', minerals: 'Quartz, Plagioclase, Biotite', outcrop: 'Yes' )
+Rock.create(nick_name: 'Quartz Arenite', type: 'Sedimentary', minerals: 'Quartz', outcrop: 'Yes' )
+Rock.create(nick_name: 'Granite', type: 'Igneous', minerals: 'Quartz, Plagioclase, Hornblende', outcrop: 'Yes' )
+Rock.create(nick_name: 'Marble', type: 'Metamorphic', minerals: 'Calcite', outcrop: 'No' )
+Rock.create(nick_name: 'Andesite', type: 'Igneous', minerals: 'Plagioclase, Orthopyroxene', outcrop: 'Yes' )
+Rock.create(nick_name: 'Dolostone', type: 'Sedimentary', minerals: 'Dolomite', outcrop: 'Yes' )
+Rock.create(nick_name: 'Phosporia', type: 'Metamorphic', minerals: 'Quartz, Phosphite', outcrop: 'No' )
+Rock.create(nick_name: 'Greenschist', type: 'Metamorphic', minerals: 'Epidote, Chlorite, Serpentine', outcrop: 'Yes' )
+Rock.create(nick_name: 'Chondrite', type: 'Igneous', minerals: 'Olivine', outcrop: 'No' )
+Rock.create(nick_name: 'Kimberlite', type: 'Igneous', minerals: 'Olivine, Diamond, Clinopyroxene ', outcrop: 'Yes' )
+
+10.times do 
+    User.create(username: Faker::Name.unique.first_name, home_location: Faker::Address.city, professional: 'No')
+end
+
+10.times do 
+    Location.create(coordinates: )
+end
