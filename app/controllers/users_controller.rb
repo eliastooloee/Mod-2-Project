d85d56aecd6b5fc220eddfe73e04d5f4fe452311
eliastooloee <elitooloee@gmail.com>
2020-01-24 07:33:17 -0800

    Changed so that users can only edit rocks they created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :home_location, :professional)
+        params.require(:user).permit(:username, :home_location, :professional, rock_ids: [])
     end
 
     def find_user 
