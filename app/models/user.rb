f80c4b2a23e052cbf83d905b0f87428a275d4d6c
eliastooloee <elitooloee@gmail.com>
2020-01-24 13:07:05 -0800

    added validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :user_rocks
     has_many :rocks, through: :user_rocks
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
